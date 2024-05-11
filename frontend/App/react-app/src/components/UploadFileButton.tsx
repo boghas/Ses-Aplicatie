7244a69a0b4bac0e54131982724fc85d2dc3ed22
boghas <bogdan.hasan@mindit.io>
2024-05-11 22:19:00 +0300

    added UploadFileButton.tsx

--- frontend/App/react-app/src/components/UploadFileButton.tsx
+++ frontend/App/react-app/src/components/UploadFileButton.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const UploadButton = ({ clientId }) => {
+const UploadButton = ({ client_id: number, endpoint: string }) => {
   const [selectedFile, setSelectedFile] = useState(null);
 
   const handleFileChange = (e) => {
@@ -15,7 +15,7 @@ const UploadButton = ({ clientId }) => {
     formData.append('new_serie_smart_meter', selectedFile);
 
     try {
-      const response = await axios.put(`http://localhost:8000/upload_serie_smart_meter/1`, formData, {
+      const response = await axios.put(`http://localhost:8000/${endpoint}/${client_id}`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
